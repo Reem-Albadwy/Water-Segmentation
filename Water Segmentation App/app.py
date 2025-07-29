66c87c38ec0c961ef94db733f701053f90fa093b
Reem-Albadwy <158544054+Reem-Albadwy@users.noreply.github.com>
2025-07-29 21:25:29 +0300

    Update app.py

--- Water Segmentation App/app.py
+++ Water Segmentation App/app.py
@@ -36,6 +36,13 @@ if not os.path.exists(MODEL_PATH):
 model = load_model(MODEL_PATH, compile=False, custom_objects={'swish': tf.nn.swish, 'FixedDropout': FixedDropout})
 
 @app.route('/', methods=['GET', 'POST'])
+def calculate_water_index(img):
+    R = img[:, :, 3].astype(np.float32)
+    G = img[:, :, 2].astype(np.float32)
+    B = img[:, :, 1].astype(np.float32)
+    water_index = (G - R) / (G + R + 1e-5)
+    return np.expand_dims(water_index, axis=-1)
+
 def index():
     result = None
     pred_result = None
@@ -43,19 +50,21 @@ def index():
     if request.method == 'POST':
         file = request.files['image']
         if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
 
-            image = load_img(filepath, target_size=(128, 128))
-            image = img_to_array(image)
-            image = image / 255.0
-            image = np.expand_dims(image, axis=0)
+            img = np.array(Image.open(filepath))
+            water_index = calculate_water_index(img)
+            img = np.concatenate([img, water_index], axis=-1)  # (128,128,13)
+            img = np.expand_dims(img, axis=0)  # (1,128,128,13)
 
-            prediction = model.predict(image)[0]
+            prediction = model.predict(img)[0]
             prediction = (prediction > 0.5).astype(np.uint8) * 255
             prediction = cv2.resize(prediction, (128, 128))
 
-            pred_path = os.path.join(app.config['PREDICTED_FOLDER'], 'pred_' + file.filename)
+            pred_filename = 'pred_' + filename.rsplit('.', 1)[0] + '.png'
+            pred_path = os.path.join(app.config['PREDICTED_FOLDER'], pred_filename)
             cv2.imwrite(pred_path, prediction)
 
             result = filepath
